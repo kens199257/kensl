a08b8cd9b7caf116d943e9667bcb4b14018d2c34
Roshan Lakmal <roshanrox911@gmail.com>
2017-12-10 02:33:10 +0530

    Adding Loader and error alerts

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -4,6 +4,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SigninPage } from '../signin/signin';
 import { NgForm } from '@angular/forms/src/directives/ng_form';
 import { AuthService } from '../../services/auth';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+
 
 @IonicPage()
 @Component({
@@ -12,16 +15,34 @@ import { AuthService } from '../../services/auth';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthService) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private authService: AuthService,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController) {
   }
   
   onSignup(form: NgForm){
+    const loading = this.loadingCtrl.create({
+      content: 'Signing you up...'
+    });
+    loading.present();
     this.authService.signup(form.value.email, form.value.password)
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+      .then(data => {
+        loading.dismiss();
+      })
+      .catch(error => {
+        loading.dismiss();
+        const alert = this.alertCtrl.create({
+          title: 'Signup failed!',
+          message: error.message,
+          buttons: ['Ok']
+        });
+        alert.present();
+      });
   }
 
-
+  
   onGoToSignIn(){
     this.navCtrl.pop();
   }
