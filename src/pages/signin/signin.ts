8c0ffd8e5bd6e5694c792569801ea330f8f65442
Roshan Lakmal <roshanrox911@gmail.com>
2017-12-10 01:56:12 +0530

    Adding onSignin method to the signin.ts

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { SignupPage } from '../signup/signup';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @IonicPage()
 @Component({
@@ -13,6 +14,9 @@ export class SigninPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  onSignin(form: NgForm){
+    console.log(form.value);
+  }
   onGoToSignUp(){
     this.navCtrl.push(SignupPage);
   }
